9a028746cf6f945b12a503c75bbba92cbbe1ca83
Peter Streef <p.streef@gmail.com>
2024-06-27 14:12:02 +0200

    change name

--- src/test/java/moderne/ClassHierarchyTest.java
+++ src/test/java/moderne/ClassHierarchyTest.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.yourorg;
+package moderne;
 
-import com.yourorg.table.ClassHierarchyReport;
+import moderne.table.ClassHierarchyReport;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
